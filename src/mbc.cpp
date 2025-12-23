a896e243c588815414fc70eeedba841683a8195d
Bogi525 <bogithe525@gmail.com>
2025-12-23 16:32:21 +0100

    Finished up MBC0 and MBC1 read/write implementations

--- src/mbc.cpp
+++ src/mbc.cpp
@@ -4,17 +4,27 @@ MBC0::MBC0(vector<uint8_t>& rom, vector<uint8_t>& ram) : rom(rom), ram(ram) {}
 
 uint8_t MBC0::read(uint16_t addr) { // 2 ROM banks
     if (addr <= SWITCHABLE_ROM_END) {
-        return rom[addr];
+        if (addr < rom.size()) {
+            return rom[addr];
+        }
+        return DEFAULT_READ_RETURN;
     }
     else if (addr >= SWITCHABLE_RAM_START && addr <= SWITCHABLE_RAM_END && !ram.empty()) {
-        return ram[addr - SWITCHABLE_RAM_START];
+        size_t offset = addr - SWITCHABLE_RAM_START;
+        if (offset < ram.size()) {
+            return ram[offset];
+        }
+        return DEFAULT_READ_RETURN;
     }
     return DEFAULT_READ_RETURN;
 }
 
 void MBC0::write(uint16_t addr, uint8_t val) {
     if (addr >= SWITCHABLE_RAM_START && addr <= SWITCHABLE_RAM_END) {
-        ram[addr - SWITCHABLE_RAM_START] = val;
+        size_t offset = addr - SWITCHABLE_RAM_START;
+        if (offset < ram.size()) {
+            ram[offset] = val;
+        }
     }
 }
 
@@ -24,13 +34,42 @@ MBC1::MBC1(vector<uint8_t>& rom, vector<uint8_t>& ram) : rom(rom), ram(ram) {
 
 uint8_t MBC1::read(uint16_t addr) {
     if (addr <= STATIC_ROM_END) {
-        return rom[addr];
+        uint8_t bank = 0;
+
+        if (banking_mode == 1) {
+            bank = current_rom_bank_high << 5;
+        }
+        bank %= rom_banks;
+
+        size_t offset = bank * STATIC_ROM_SIZE + (addr - STATIC_ROM_START);
+        if (offset < rom.size()) {
+            return rom[offset]; 
+        }
+        return DEFAULT_READ_RETURN;
     }
     else if (addr <= SWITCHABLE_ROM_END) {
-        return rom[current_rom_bank_low * SWITCHABLE_ROM_SIZE + (addr - SWITCHABLE_ROM_START)];
+        uint8_t bank = 0;
+
+        bank = current_rom_bank_high << 5 | current_rom_bank_low;
+        bank %= rom_banks;
+        if ((bank & MBC1_ROM_BANKS_MASK) == 0) {
+            bank += 1;
+        }
+
+        size_t offset = bank * SWITCHABLE_ROM_SIZE + (addr - SWITCHABLE_ROM_START);
+        if (offset < rom.size()) {
+            return rom[offset]; 
+        }
+        return DEFAULT_READ_RETURN;
     }
-    else if (addr >= SWITCHABLE_RAM_START && addr <= SWITCHABLE_RAM_END) {
-        return ram[current_ram_bank * SWITCHABLE_RAM_SIZE + (addr - SWITCHABLE_RAM_START)];
+    else if (addr >= SWITCHABLE_RAM_START && addr <= SWITCHABLE_RAM_END && ram_enabled) {
+        uint8_t bank = (banking_mode == 0) ? 0 : current_ram_bank;
+        
+        size_t offset = bank * SWITCHABLE_RAM_SIZE + (addr - SWITCHABLE_RAM_START);
+        if (offset < ram.size()) {
+            return ram[offset]; 
+        }
+        return DEFAULT_READ_RETURN;
     }
     return DEFAULT_READ_RETURN; 
 }
@@ -42,24 +81,20 @@ void MBC1::write(uint16_t addr, uint8_t val) {
     else if (addr <= ROM_BANK_SELECT_END) {
         current_rom_bank_low = val & MBC1_ROM_BANKS_MASK;
         if (current_rom_bank_low == 0) current_rom_bank_low = 1;
-        current_rom_bank_low %= rom_banks;
     }
     else if (addr <= RAM_BANK_SELECT_END) {
-        if (!banking_mode) {
-            current_rom_bank_high = val & 0x03;
-        }
-        else {
-            current_ram_bank = val & MBC1_RAM_BANKS_MASK;
-        }
+        current_rom_bank_high = val & MBC1_RAM_BANKS_MASK;
+        current_ram_bank = val & MBC1_RAM_BANKS_MASK;
     }
     else if (addr <= BANKING_MODE_END) {
         banking_mode = val & 0x01;
     }
-    else if (addr >= SWITCHABLE_RAM_START && addr <= SWITCHABLE_RAM_END) {
-        if (!banking_mode) {
-            ram[addr - SWITCHABLE_RAM_START] = val;
-        } else {
-            ram[current_ram_bank * SWITCHABLE_RAM_SIZE + (addr - SWITCHABLE_RAM_START)] = val;
+    else if (addr >= SWITCHABLE_RAM_START && addr <= SWITCHABLE_RAM_END && ram_enabled) {
+        uint8_t bank = (banking_mode == 0) ? 0 : current_ram_bank;
+        
+        size_t offset = bank * SWITCHABLE_RAM_SIZE + (addr - SWITCHABLE_RAM_START);
+        if (offset < ram.size()) {
+            ram[offset] = val;
         }
     }
 }
